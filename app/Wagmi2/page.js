07b34ad9b5657228ac4a64311bfe5fba5643384f
apophis51 <malcolmxvernon@hotmail.com>
2023-10-09 23:47:55 -0400

    update

---
+++ app/Wagmi2/page.js
@@ -0,0 +1,62 @@
+'use client'
+
+import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
+
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+require("dotenv").config();
+
+
+import { Profile } from './Profile'
+
+
+// Configure chains & providers with the Alchemy provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY }), publicProvider()],
+)
+
+// Set up wagmi config
+const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: '...',
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+})
+
+// Pass config to React Context Provider
+export default function App() {
+  return (
+    <WagmiConfig config={config}>
+      <Profile />
+    </WagmiConfig>
+  )
+}
